69cac94bd358241c510b76be68b314c1e17fe837
Ritesh983 <ritesh20040214@gmail.com>
2025-07-14 18:49:48 +0530

    tailwind done

---
+++ 02-vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+function App() {
+    const username="Ritesh"
+  return (
+    <>
+    <Chai />
+    <h1>chai aur react {username}</h1>
+    {/* username is evaluated expression */}
+    </>
+  
+  )
+}
+
+export default App
